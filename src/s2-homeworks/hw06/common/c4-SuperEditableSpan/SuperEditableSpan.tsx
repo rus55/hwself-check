108aafeb90a457258e60350f9642c6eabd21ce6c
rus55 <r-mit@mail.ru>
2023-01-13 16:56:41 +0300

    sixth task

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -42,20 +42,22 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
 
     const onEnterCallback = () => {
         // выключить editMode при нажатии Enter // делают студенты
+       setEditMode(!editMode)
+       // onEnter?.()
 
-        onEnter?.()
+
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
         // выключить editMode при нажатии за пределами инпута // делают студенты
-
-        onBlur?.(e)
+        setEditMode(!editMode)
+        // onBlur?.(e)
     }
     const onDoubleClickCallBack = (
         e: React.MouseEvent<HTMLSpanElement, MouseEvent>
     ) => {
         // включить editMode при двойном клике // делают студенты
-
-        onDoubleClick?.(e)
+        setEditMode(!editMode)
+        // onDoubleClick?.(e)
     }
 
     const spanClassName = s.span
